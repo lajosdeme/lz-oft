6a19e134e9c8ce8af60ce74847cd86778909d392
lajosdeme <44027725+lajosdeme@users.noreply.github.com>
2024-07-14 02:20:00 +0200

    setup oft for use with foundry

---
+++ src/PacketV1Codec.sol
@@ -0,0 +1,153 @@
+// SPDX-License-Identifier: LZBL-1.2
+
+pragma solidity ^0.8.20;
+
+struct Packet {
+    uint64 nonce;
+    uint32 srcEid;
+    address sender;
+    uint32 dstEid;
+    bytes32 receiver;
+    bytes32 guid;
+    bytes message;
+}
+
+library AddressCast {
+    error AddressCast_InvalidSizeForAddress();
+    error AddressCast_InvalidAddress();
+
+    function toBytes32(bytes calldata _addressBytes) internal pure returns (bytes32 result) {
+        if (_addressBytes.length > 32) revert AddressCast_InvalidAddress();
+        result = bytes32(_addressBytes);
+        unchecked {
+            uint256 offset = 32 - _addressBytes.length;
+            result = result >> (offset * 8);
+        }
+    }
+
+    function toBytes32(address _address) internal pure returns (bytes32 result) {
+        result = bytes32(uint256(uint160(_address)));
+    }
+
+    function toBytes(bytes32 _addressBytes32, uint256 _size) internal pure returns (bytes memory result) {
+        if (_size == 0 || _size > 32) revert AddressCast_InvalidSizeForAddress();
+        result = new bytes(_size);
+        unchecked {
+            uint256 offset = 256 - _size * 8;
+            assembly {
+                mstore(add(result, 32), shl(offset, _addressBytes32))
+            }
+        }
+    }
+
+    function toAddress(bytes32 _addressBytes32) internal pure returns (address result) {
+        result = address(uint160(uint256(_addressBytes32)));
+    }
+
+    function toAddress(bytes calldata _addressBytes) internal pure returns (address result) {
+        if (_addressBytes.length != 20) revert AddressCast_InvalidAddress();
+        result = address(bytes20(_addressBytes));
+    }
+}
+
+library PacketV1Codec {
+    using AddressCast for address;
+    using AddressCast for bytes32;
+
+    uint8 internal constant PACKET_VERSION = 1;
+
+    // header (version + nonce + path)
+    // version
+    uint256 private constant PACKET_VERSION_OFFSET = 0;
+    //    nonce
+    uint256 private constant NONCE_OFFSET = 1;
+    //    path
+    uint256 private constant SRC_EID_OFFSET = 9;
+    uint256 private constant SENDER_OFFSET = 13;
+    uint256 private constant DST_EID_OFFSET = 45;
+    uint256 private constant RECEIVER_OFFSET = 49;
+    // payload (guid + message)
+    uint256 private constant GUID_OFFSET = 81; // keccak256(nonce + path)
+    uint256 private constant MESSAGE_OFFSET = 113;
+
+    function encode(Packet memory _packet) internal pure returns (bytes memory encodedPacket) {
+        encodedPacket = abi.encodePacked(
+            PACKET_VERSION,
+            _packet.nonce,
+            _packet.srcEid,
+            _packet.sender.toBytes32(),
+            _packet.dstEid,
+            _packet.receiver,
+            _packet.guid,
+            _packet.message
+        );
+    }
+
+    function encodePacketHeader(Packet memory _packet) internal pure returns (bytes memory) {
+        return
+            abi.encodePacked(
+                PACKET_VERSION,
+                _packet.nonce,
+                _packet.srcEid,
+                _packet.sender.toBytes32(),
+                _packet.dstEid,
+                _packet.receiver
+            );
+    }
+
+    function encodePayload(Packet memory _packet) internal pure returns (bytes memory) {
+        return abi.encodePacked(_packet.guid, _packet.message);
+    }
+
+    function header(bytes calldata _packet) internal pure returns (bytes calldata) {
+        return _packet[0:GUID_OFFSET];
+    }
+
+    function version(bytes calldata _packet) internal pure returns (uint8) {
+        return uint8(bytes1(_packet[PACKET_VERSION_OFFSET:NONCE_OFFSET]));
+    }
+
+    function nonce(bytes calldata _packet) internal pure returns (uint64) {
+        return uint64(bytes8(_packet[NONCE_OFFSET:SRC_EID_OFFSET]));
+    }
+
+    function srcEid(bytes calldata _packet) internal pure returns (uint32) {
+        return uint32(bytes4(_packet[SRC_EID_OFFSET:SENDER_OFFSET]));
+    }
+
+    function sender(bytes calldata _packet) internal pure returns (bytes32) {
+        return bytes32(_packet[SENDER_OFFSET:DST_EID_OFFSET]);
+    }
+
+    function senderAddressB20(bytes calldata _packet) internal pure returns (address) {
+        return sender(_packet).toAddress();
+    }
+
+    function dstEid(bytes calldata _packet) internal pure returns (uint32) {
+        return uint32(bytes4(_packet[DST_EID_OFFSET:RECEIVER_OFFSET]));
+    }
+
+    function receiver(bytes calldata _packet) internal pure returns (bytes32) {
+        return bytes32(_packet[RECEIVER_OFFSET:GUID_OFFSET]);
+    }
+
+    function receiverB20(bytes calldata _packet) internal pure returns (address) {
+        return receiver(_packet).toAddress();
+    }
+
+    function guid(bytes calldata _packet) internal pure returns (bytes32) {
+        return bytes32(_packet[GUID_OFFSET:MESSAGE_OFFSET]);
+    }
+
+    function message(bytes calldata _packet) internal pure returns (bytes calldata) {
+        return bytes(_packet[MESSAGE_OFFSET:]);
+    }
+
+    function payload(bytes calldata _packet) internal pure returns (bytes calldata) {
+        return bytes(_packet[GUID_OFFSET:]);
+    }
+
+    function payloadHash(bytes calldata _packet) internal pure returns (bytes32) {
+        return keccak256(payload(_packet));
+    }
+}
